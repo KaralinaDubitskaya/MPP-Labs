2b726e3bad3f92a9dcce3995dc2c18e0f754b072
Karolina Dubitskaya <Karolina.D100899@gmail.com>
2018-09-13 21:03:59 +0300

    Add TaskQueue project (lab #1)

---
+++ TaskQueue (lab1)/TaskQueue/BlockingQueue.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskQueue
+{
+    public class BlockingQueue<T> : Queue<T>
+    {
+        private object SyncRoot = new object();
+
+        public bool IsEmpty { get { return (this.Count == 0); } }
+
+        public BlockingQueue() : base() { }
+
+        // add item to the queue
+        public new void Enqueue(T item)
+        {
+            lock (SyncRoot)
+            {
+                base.Enqueue(item);
+            }
+        }
+
+        // get item from the queue
+        public new T Dequeue()
+        {
+            T item = default(T);
+
+            lock (SyncRoot)
+            {
+                if (!IsEmpty)
+                {
+                    item = base.Dequeue();
+                }
+            }
+
+            return item;
+        }
+    }
+}
---
+++ TaskQueue (lab1)/TaskQueue/TaskQueue.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace TaskQueue
+{
+    public class TaskQueue
+    {
+        // task queue
+        private BlockingQueue<TaskDelegate> tasks;
+
+        public TaskQueue(byte count)
+        {
+            tasks = new BlockingQueue<TaskDelegate>();
+
+            // create and start *count* threads 
+            for (int i = 0; i < count; i++)
+            {
+                var thread = new Thread(PerformTask);
+                thread.Name = "Thread" + i;
+                thread.IsBackground = true;
+                thread.Start();
+            }
+        }
+
+        // type of tasks processed by the threads
+        public delegate void TaskDelegate();
+
+        // enqueue task for execution 
+        public void EnqueueTask(TaskDelegate task)
+        {
+            tasks.Enqueue(task);
+        }
+
+        // method to be invoked when a thread begins executing
+        private void PerformTask()
+        {
+            while (true)
+            {
+                while (tasks.IsEmpty) { }
+
+                // get another task
+                TaskDelegate task = tasks.Dequeue();
+
+                if (task == null) { continue; }
+
+                try
+                {
+                    // execute the task
+                    task();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                }
+            }
+        }
+    }
+}
