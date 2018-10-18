d20eed8638288b0cde92eef26b0fc63ccd6d349a
Karalina Dubitskaya <dubitskaya.karalina@gmail.com>
2018-10-19 00:54:43 +0300

    Add AssemblyLoader (lab 4)

---
+++ Labs/AssemblyLoader/AssemblyInfo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyLoader
+{
+    public class AssemblyInfo
+    {
+        private Assembly _assembly;
+
+        public void LoadAssembly(string path)
+        {
+            string fileExtention = Path.GetExtension(path);
+
+            try
+            {
+                _assembly = Assembly.LoadFrom(path);
+            }
+            catch (ArgumentException)
+            {
+                throw new ArgumentException("Please, enter full path to the assembly.");
+            }
+            catch (FileNotFoundException)
+            {
+                throw new FileNotFoundException($"Unable to find the file {path}.");
+            }
+            catch (BadImageFormatException)
+            {
+                throw new BadImageFormatException($"The file {path} is invalid assembly.");
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        // Return array that contains info about public types of the assembly
+        // sorted by the namespace and by the name
+        public Type[] GetPublicTypes()
+        {
+            return _assembly.GetTypes()
+                .Where(type => type.IsPublic)
+                .OrderBy(type => type.Namespace + type.Name)
+                .ToArray();
+        }
+    }
+}
---
+++ Labs/AssemblyLoader/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyLoader
+{
+    class Program
+    {
+        private const byte ARG_COUNT = 1;
+
+        public static void Main(string[] args)
+        {
+            if (args.Length != ARG_COUNT)
+            {
+                Console.WriteLine("Error: invalid input.");
+                Console.WriteLine("Usage: AssemblyLoader.exe 'full_path_to_the_assembly'");
+                return;
+            }
+
+            string path = args[0];
+            
+            var assembly = new AssemblyInfo();
+            try
+            {
+                assembly.LoadAssembly(path);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+
+            var types = assembly.GetPublicTypes();
+            foreach (var type in types)
+            {
+                PrintTypeInfo(type);
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void PrintTypeInfo(Type type)
+        {
+            Console.WriteLine(type.Namespace + '.' + type.Name);
+        }
+    }
+}
